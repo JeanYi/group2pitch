408ba3e2c3e8a207bd158dea9fb285c9c536873f
Jean <jeanyi.st@gmail.com>
2018-07-08 09:51:34 +0800

    completed homepage styling, pending inclusion of url links

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,29 @@
 module SessionsHelper
+ # # Returns true if the user is logged in, false otherwise.
+   def logged_in?
+       !current_client.nil?
+   end
+
+   # Confirms a signed_in client 
+   def logged_in_client 
+      unless logged_in?
+         flash[:danger] = "Please log in."
+         redirect_to sign_in_path 
+      end
+   end
+
+ # Returns true if the trainer is logged in, false otherwise.
+   def trainer_logged_in? 
+   	   !current_trainer.nil?
+   end
+
+   # Confirms a signed_in client 
+   def logged_in_trainer 
+      unless trainer_logged_in?
+         flash[:danger] = "Please log in."
+         redirect_to sign_in_trainer_path 
+      end
+   end
+
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,29 @@
 Rails.application.routes.draw do
 
-root 'trainers#new'
+root 'pages#index'
 
 resources :trainers
 resources :listings
+resources :pages 
   
 resources :clients do 
-  	resources :enquiries
+  	resources :enquiries 
     end
  
+
+  get "/" => "pages#index" 
+
 
   get "/sign_in_trainer" => "sessions#new_trainer", as: "sign_in_trainer"
   delete "/sign_out_trainer" => "sessions#destroy_trainer", as: "sign_out_trainer"
   post "/sessions_trainer" => "sessions#create_trainer" 
+
+
+
   get "/sign_in" => "sessions#new", as: "sign_in"
   delete "/sign_out" => "sessions#destroy", as: "sign_out"
   post "/sessions" => "sessions#create" 
+  
   patch "/clients" => "clients#update"
   patch "/listings" => "listings#update"
 
